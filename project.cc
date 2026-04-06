b4a361258eaf14cfb2a81d394d39ea663a998b86
MBlanc24 <116228424+MBlanc24@users.noreply.github.com>
2026-04-05 17:33:13 -0700

    Implement TCP variant selection and adjust link delay
    Added TCP variant selection and modified satellite link delay.

--- project.cc
+++ project.cc
@@ -1,5 +1,3 @@
-
-
 #include "ns3/applications-module.h"
 #include "ns3/core-module.h"
 #include "ns3/internet-module.h"
@@ -19,10 +17,30 @@ NS_LOG_COMPONENT_DEFINE("PROJECT");
 int
 main(int argc, char* argv[])
 {
+
+    std::string tcpType = "cubic";
+
     CommandLine cmd(__FILE__);
+    cmd.AddValue("tcpType", "TCP variant: cubic or bbr", tcpType);
     cmd.Parse(argc, argv);
 
     Time::SetResolution(Time::NS);
+
+    //TCP CUBIC
+    if (tcpType == "cubic") {
+        Config::SetDefault("ns3::TcpL4Protocol::SocketType",
+            TypeIdValue(TcpCubic::GetTypeId()));
+    }
+
+    //TCP BBR
+    else if (tcpType == "bbr") {
+        Config::SetDefault("ns3::TcpL4Protocol::SocketType",
+            TypeIdValue(TcpBbr::GetTypeId()));
+    }
+
+    else {
+        NS_FATAL_ERROR("Invalid tcpType. Use --tcpType=cubic or --tcpType=bbr");
+    }
 
 	NS_LOG_INFO("Creating Topology"); //added
 
@@ -43,7 +61,9 @@ main(int argc, char* argv[])
     // Satellite link - used for router to router
     PointToPointHelper satLink;
     satLink.SetDeviceAttribute ("DataRate", StringValue ("10Mbps"));
-    satLink.SetChannelAttribute ("Delay", StringValue ("300ms"));
+    satLink.SetChannelAttribute ("Delay", StringValue ("100ms"));
+    //DropTail queue
+    satLink.SetQueue("ns3::DropTailQueue", "MaxSize", StringValue("100p"));
 
     //installing links for the routers
     NetDeviceContainer d1 = gndLink.Install(senderRouter);
@@ -63,24 +83,36 @@ main(int argc, char* argv[])
     Ipv4InterfaceContainer i3 = address.Assign(d3);
 
     Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+    
 
-    //receiver tcp
+    //receiver tcp socket
     PacketSinkHelper sink("ns3::TcpSocketFactory",
-                      InetSocketAddress(Ipv4Address::GetAny(), 8080));
+        InetSocketAddress(Ipv4Address::GetAny(), 8080));
 
     ApplicationContainer sinkApp = sink.Install(nodes.Get(3));
     sinkApp.Start(Seconds(0.0));
     sinkApp.Stop(Seconds(60.0));
 
-    //sender tcp
+    //sender tcp socket
     BulkSendHelper source("ns3::TcpSocketFactory",
-                      InetSocketAddress(i3.GetAddress(1), 8080));
+        InetSocketAddress(i3.GetAddress(1), 8080));
 
     source.SetAttribute("MaxBytes", UintegerValue(0));
 
     ApplicationContainer sourceApp = source.Install(nodes.Get(0));
     sourceApp.Start(Seconds(1.0));
     sourceApp.Stop(Seconds(60.0));
+
+
+    //RateErrorModel simulates wireless loss
+    Ptr<RateErrorModel> em = CreateObject<RateErrorModel>();
+    em->SetAttribute("ErrorRate", DoubleValue(1e-5));
+
+    //packet loss only on one side of the satellite link reciever
+    d2.Get(1)->SetAttribute("ReceiveErrorModel", PointerValue(em));
+
+
+
 
     Simulator::Run();
     Simulator::Destroy();
